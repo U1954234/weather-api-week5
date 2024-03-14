d8af2e610a7a65d61a1e4b519a5a79e0e9077ef4
U1954234 <162350063+U1954234@users.noreply.github.com>
2024-03-14 14:52:42 +0000

    1# Please enter the commit message for your changes. Lines starting

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import weatherRoute from "./routes/weatherRoutes.js";
 import cors from "cors";
 
 // We will create an express app
@@ -10,7 +11,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors())
 
-app.use("/api/weather", 'weatherRoute');
+app.use("/api/weather", weatherRoute);
 
  // Start the express server
 app.listen(PORT, () => {
